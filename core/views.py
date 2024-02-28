d2bb8e959fcf713466c4785d6efe5915afed307b
opkpEh <harshpandeykp20@gmail.com>
2024-02-29 01:56:48 +0530

    basic user database added

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile
 
 # Create your views here.
 
@@ -10,4 +13,33 @@ def profile(request):
     return render(request, 'profile.html',)
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if(password==password2):
+            if(User.objects.filter(email=email).exists()):
+                messages.info(request, "Email already used")
+                return redirect('signup')
+            
+            elif(User.objects.filter(username=username).exists()):
+                messages.info(request, "Username already used")
+                return redirect('signup')
+
+            else:
+                user= User.objects.create_user(username= username, email= email, password= password)
+                user.save()
+            
+                user_model= User.objects.get(username= username)
+                new_profile= Profile(user= user_model, id_user= user_model.id)
+                new_profile.save()
+                return redirect('index')
+
+        else:
+            messages.info(request, "Password not matching")
+            return redirect('signup')
+
+    else:
+        return render(request, 'signup.html')
